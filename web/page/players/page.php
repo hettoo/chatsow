f973938d338b07b7afec1c7ce00b421672c555ac
Gerco van Heerdt <gercovanheerdt@gmail.com>
2013-03-28 17:51:59 +0100

    Note
    Signed-off-by: Gerco van Heerdt <gercovanheerdt@gmail.com>

--- web/page/players/page.php
+++ web/page/players/page.php
@@ -6,20 +6,9 @@ import_lib('Pager');
 
 $players = '';
 $i = 0;
-$pager = new Pager($hierarchy[2] - 1, $shared['max_rows'] * COLUMNS, "SELECT `id`, `name` FROM `player` ORDER BY `last_seen` DESC");
-$rows = $pager->getRows();
-foreach ($rows as $row) {
-	if ($i == 0)
-		$players .= '<tr>';
-	$players .= '<td>' . format_player($row['name'], $row['id']) . '</td>';
-	$i = ($i + 1) % COLUMNS;
-	if ($i == 0)
-		$players .= '</tr>';
-}
-if ($i != 0)
-	$players .= '</tr>';
-
 ?>
+
+Not implemented yet.
 <?= format_pages(2, $pager); ?>
 <table>
 <?= $players; ?>
