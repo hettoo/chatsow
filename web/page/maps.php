de2684d340e60ccf6c8a300700e061516d621f23
Gerco van Heerdt <gercovanheerdt@gmail.com>
2013-03-29 16:49:07 +0100

    Switched two columns
    Signed-off-by: Gerco van Heerdt <gercovanheerdt@gmail.com>

--- web/page/maps.php
+++ web/page/maps.php
@@ -7,12 +7,12 @@ $pager = new Pager($hierarchy[1] - 1, $shared['max_rows'], "SELECT `name`, `reco
 $maps = '';
 $rows = $pager->getRows();
 foreach ($rows as $row) {
-	$maps .= '<tr><td>' . format_map($row['name']) . '</td><td>' . format_time($row['record']) . '</td><td>' . $row['record_holder'] . '</td></tr>';
+	$maps .= '<tr><td>' . format_map($row['name']) . '</td><td>' . format_player($row['record_holder']) . '</td><td>' . format_time($row['record']) . '</td></tr>';
 }
 
 ?>
 <?= format_pages(1, $pager); ?>
 <table>
-	<tr><th>Map</th><th>Record</th><th>Recordholder</th></tr>
+	<tr><th>Map</th><th>Record holder</th><th>Record</th></tr>
 	<?= $maps; ?>
 </table>
