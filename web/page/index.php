f2c88fd8a0d411c8d59cb665ea39e67f1ff1614a
Gerco van Heerdt <gercovanheerdt@gmail.com>
2013-04-05 15:29:56 +0200

    New notification
    Signed-off-by: Gerco van Heerdt <gercovanheerdt@gmail.com>

--- web/page/index.php
+++ web/page/index.php
@@ -7,8 +7,7 @@ while ($row = $result->fetch_array())
 
 ?>
 <p>
-<font color="red">Sorry, but until the server issues get fixed I won't be running this anymore.</font>
-It was a nice experiment so far though.
+<font color="red">Because of server crashes I have setup my own server containing this bot until I figure out how to fix the issues.</font>
 </p>
 <p>
 This is the webinterface of an instance of the <a href="http://github.com/hettoo/chatsow" target="_blank">chatsow</a> project, a <a href="http://warsow.net" target="_blank">Warsow</a> chat client with specific plugin functionality for <a href="http://mgxrace.com" target="_blank">MGX Race</a> created by <?= format_player('^7^0/^7inc^2.^7hettoo^0/', -1); ?>.
