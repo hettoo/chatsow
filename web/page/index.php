3e290669ddbc5d6ae89afba61786a0f3d255ce0b
Gerco van Heerdt <gercovanheerdt@gmail.com>
2013-04-30 00:04:05 +0200

    Global stats
    Signed-off-by: Gerco van Heerdt <gercovanheerdt@gmail.com>

--- web/page/index.php
+++ web/page/index.php
@@ -5,6 +5,15 @@ $result = $db->query("SELECT P.`id`, M.`name`, `record`, P.`name` AS `record_hol
 while ($row = $result->fetch_array())
     $maps .= '<tr><td>' . format_map($row['name']) . '</td><td>' . format_player($row['record_holder'], $row['id'], -1) . '</td><td class="right">' . format_time($row['record'], $row['name']) . '</td></tr>';
 
+$map_count = 0;
+$result = $db->query("SELECT COUNT(*) AS `maps` FROM `map`") or die($db->error);
+while ($row = $result->fetch_array())
+    $map_count = $row['maps'];
+$player_count = 0;
+$result = $db->query("SELECT COUNT(*) AS `players` FROM `player`") or die($db->error);
+while ($row = $result->fetch_array())
+    $player_count = $row['players'];
+
 ?>
 <p>
 This is the webinterface of an instance of the <a href="http://github.com/hettoo/chatsow" target="_blank">chatsow</a> project created by <?= format_player('^7^0/^7inc^2.^7hettoo^0/', -1); ?>, a <a href="http://warsow.net" target="_blank">Warsow</a> chat client with specific plugin functionality for <a href="http://github.com/Racenet/racesow" target="_blank">Racesow</a>.
@@ -18,6 +27,10 @@ Some players believe that their records should not be shared.
 In my opinion, race should be a collaboration of players trying to approach the optimal time for a map together, not a place where selfish people desperately try to suppress competition.
 Eventually routes will be stolen anyway, if people really want to do this.
 Play on your own server, disconnected from the rest of the world, if you want to be sure.
+</p>
+<h2>Stats</h2>
+<p>
+A total of <b><?= $map_count; ?></b> records have been recorded from <b><?= $player_count; ?></b> different players.
 </p>
 <h2>Latest records</h2>
 <p>
