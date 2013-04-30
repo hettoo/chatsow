93fa9e5607ddda245e6b7745b264d2b1ef08ee80
Gerco van Heerdt <gercovanheerdt@gmail.com>
2013-04-30 23:55:15 +0200

    No subfolder for the map pages needed
    Signed-off-by: Gerco van Heerdt <gercovanheerdt@gmail.com>

--- web/page/maps_init.php
+++ web/page/maps_init.php
@@ -1,5 +1,10 @@
 <?php
 
-init_child_page('_');
+if (count($hierarchy) == 1 && $_POST['submit']) {
+    header('Location: http://' . $_SERVER['HTTP_HOST'] . url('maps/1/' . $_POST['name']));
+    exit;
+}
+
+$shared['head'] = 'Maps';
 
 ?>
