103ecace79bb013c6a69497a981212e98d63d8ff
Gerco van Heerdt <gercovanheerdt@gmail.com>
2013-05-01 18:00:54 +0200

    Converted the player subpages
    Signed-off-by: Gerco van Heerdt <gercovanheerdt@gmail.com>

--- web/page/players/_.php
+++ web/page/players/_.php
@@ -1,31 +1,6 @@
-<?php
-
-import_lib('Pager');
-
-$id = (int)$hierarchy[1];
-$like = $db->real_escape_string($hierarchy[3]);
-
-$pager = new Pager(2, $shared['max_rows'], "`name`, `record`, UNIX_TIMESTAMP(`timestamp`) AS `timestamp` FROM `map` WHERE `player`=$id AND `name` LIKE '%$like%' ORDER BY `name`");
-
-$maps = '';
-$pager->query();
-$rows = $pager->getRows();
-foreach ($rows as $row) {
-    $maps .= '<tr><td>' . format_map($row['name']) . '</td><td class="right">' . format_time($row['record'], $row['name']) . '</td><td class="right">' . format_date($row['timestamp']) . '</td></tr>';
-}
-
-?>
 <p>
 Records below are the best runs recorded by the bot, not necessarily actual records.
 </p>
-<p>
-<form action="<?= url('players/' . $hierarchy[1]); ?>" method="POST">
-<input type="text" name="name" value="<?= $like; ?>" />
-<input type="submit" name="submit" value="Search">
-</form>
-</p>
-<?= $pager->format(); ?>
-<table>
-    <tr><th>Map</th><th class="right">Record</th><th class="right">Date</th></tr>
-    <?= $maps; ?>
-</table>
+<?= format_search($shared['like']); ?>
+<?= $shared['pager']->format(); ?>
+<?= $shared['table']->format(); ?>
--- web/page/players/__init.php
+++ web/page/players/__init.php
@@ -1,15 +1,40 @@
 <?php
 
-if (count($hierarchy) == 2 && $_POST['submit']) {
-    header('Location: http://' . $_SERVER['HTTP_HOST'] . url('players/' . $hierarchy[1] . '/1/' . $_POST['name']));
-    exit;
+import_lib('Pager');
+import_lib('Table');
+
+$id = (int)$hierarchy[1];
+
+$table = new Table(2);
+$table->addColumn(array('name' => 'name', 'title' => 'Map'));
+$table->addColumn(array('name' => 'record', 'title' => 'Record', 'align' => 'right'));
+$table->addColumn(array('name' => 'timestamp', 'title' => 'Date', 'align' => 'right'));
+
+$table->processOrder('name');
+
+$like = search_get(4);
+
+$pager = new Pager(3, $shared['max_rows'], "`name`, `record`, UNIX_TIMESTAMP(`timestamp`) AS `timestamp` FROM `map` WHERE `player`=$id AND `name` LIKE '%$like%'" . $table->getOrder());
+
+search_redirect(4, $pager, $_POST['name']);
+
+$maps = '';
+$pager->query();
+$rows = $pager->getRows();
+foreach ($rows as $row) {
+    $table->addField(format_map($row['name']));
+    $table->addField(format_time($row['record'], $row['name']));
+    $table->addField(format_date($row['timestamp']));
 }
 
-$players = '';
-$result = $db->query("SELECT `id`, `name` FROM `player` WHERE `id`=" . (int)$hierarchy[1]) or die($db->error);
+$result = $db->query("SELECT `id`, `name` FROM `player` WHERE `id`=" . $id) or die($db->error);
 while ($row = $result->fetch_array()) {
     $shared['head'] = 'Player ' . format_player($row['name'], $row['id']);
     $shared['player'] = $row;
 }
+
+$shared['like'] = $like;
+$shared['table'] = $table;
+$shared['pager'] = $pager;
 
 ?>
