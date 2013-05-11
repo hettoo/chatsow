d981d567338574595be3acbc976349ad01590b1e
Gerco van Heerdt <gercovanheerdt@gmail.com>
2013-05-11 13:18:01 +0200

    This then
    xD
    
    Signed-off-by: Gerco van Heerdt <gercovanheerdt@gmail.com>

--- source/parser.c
+++ source/parser.c
@@ -406,9 +406,9 @@ static void parse_player_state(parser_t *parser, msg_t *msg, short *old_stats, i
 
 	for( i = 0; i < PS_MAX_STATS; i++ ) {
 		if( statbits[i>>5] & ( 1<<(i&31) ) )
-			set_stat(parser->client, index == 0 ? 1 : parser->playernums[index] + 1, i, read_short(msg));
+			set_stat(parser->client, parser->playernums[index + 1], i, read_short(msg));
         else
-			set_stat(parser->client, parser->playernums[index] + 1, i, old_stats[i]);
+			set_stat(parser->client, parser->playernums[index + 1], i, old_stats[i]);
 	}
 }
 
@@ -522,15 +522,15 @@ static void parse_frame(parser_t *parser, msg_t *msg) {
     int players = 0;
     while ((cmd = read_byte(msg)) != 0) { // svc_playerinfo
         start = msg->readcount - 1;
-        parse_player_state(parser, msg, old_stats + (players == 0 ? 1 : parser->playernums[players] + 1) * PS_MAX_STATS, players);
+        parse_player_state(parser, msg, old_stats + (parser->playernums[players + 1]) * PS_MAX_STATS, players);
         backup = msg->readcount;
         msg->readcount = start;
         record_wrapped(parser, msg, backup - start, players);
         msg->readcount = backup;
         players++;
     }
-    while (players < MAX_CLIENTS) {
-        set_stat(parser->client, players == 0 ? 1 : parser->playernums[players] + 1, STAT_TEAM, 0);
+    while (players < MAX_CLIENTS - 1) {
+        set_stat(parser->client, parser->playernums[players + 1], STAT_TEAM, 0);
         players++;
     }
     msg->readcount -= 1;
