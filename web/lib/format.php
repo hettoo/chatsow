906016e9544cc291068cae7cdb40f249c259b732
Gerco van Heerdt <gercovanheerdt@gmail.com>
2013-06-27 23:10:11 +0200

    Stupid space
    Signed-off-by: Gerco van Heerdt <gercovanheerdt@gmail.com>

--- web/lib/format.php
+++ web/lib/format.php
@@ -120,7 +120,7 @@ function format_date_relative($time) {
 }
 
 function format_date($time) {
-    return '<span class="time">' . exact_date($time) . ($time == 0 ? '' : ' <span class="exacttime">' . exact_time($time) . '</span>') . '</span>';
+    return '<span class="time">' . exact_date($time) . ($time == 0 ? '' : '<span class="exacttime">' . exact_time($time) . '</span>') . '</span>';
 }
 
 function format_server($server) {
