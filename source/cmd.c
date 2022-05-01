a9164b0e8e353250b5721a5552c11e75a7012a13
Gerco van Heerdt <gercovanheerdt@gmail.com>
2022-05-01 15:00:33 +0100

    Overflow fix

--- source/cmd.c
+++ source/cmd.c
@@ -24,7 +24,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "client.h"
 #include "cmd.h"
 
-#define CMD_STACK_SIZE 16
+#define CMD_STACK_SIZE 64
 
 typedef enum cmd_type_e {
     CT_NORMAL,
